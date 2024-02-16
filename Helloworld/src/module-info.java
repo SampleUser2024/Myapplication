49cd4350a5c164fcf5b9cdafbfbd6ffb755c8d0a
SampleUser2024 <hellosampleuser237@gmail.com>
2024-02-15 20:17:12 -0600

    Made changes to the code

--- Helloworld/src/module-info.java
+++ Helloworld/src/module-info.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
 
 public class HelloWorld
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Hello Testing");
-		System.out.println("This is for Testing");
+	    int N = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value for N:");
+        N = scanner.nextInt();
+        int arr[] = new int[N];
+        for (int k = 0; k < N; k++) {
+            System.out.println("The array elements are:");
+            arr[k] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+        System.out.println("----------------");
+        System.out.println(arr[N-1]);
+        System.out.println(arr[N-2]);
 	}
 
 }
