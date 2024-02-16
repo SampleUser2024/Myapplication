0a88a08bd7f45ba47309ac6720724ea1f13a4c03
SampleUser2024 <hellosampleuser237@gmail.com>
2024-02-15 21:04:46 -0600

    Changed logic

--- Helloworld/src/module-info.java
+++ Helloworld/src/module-info.java
@@ -5,29 +5,30 @@ public class HelloWorld
 
 	public static void main(String[] args)
 	{
-	    int N = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the value for N:");
-        N = scanner.nextInt();
-        int arr[] = new int[N];
-        for (int k = 0; k < N; k++) {
-            System.out.println("The array elements are:");
-            arr[k] = scanner.nextInt();
-        }
+		 Scanner scanner = new Scanner(System.in);
+	        int T = scanner.nextInt();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
+	        for (int i = 0; i < T; i++) {
 
-        }
-        System.out.println("-------");
-        System.out.println(arr[N-1]);
-        System.out.println(arr[N-2]);
+	            int N = scanner.nextInt();
+	            int arr[] = new int[N];
+	            for (int k = 0; k < arr.length; k++) {
+	                arr[k] = scanner.nextInt();
+	            }
+	            boolean isincreasing = true;
+	            for (int j = 1; j < arr.length; j++) {
+	                if (arr[j - 1] > arr[j]) {
+	                    isincreasing = false;
+	                    break;
+	                }
+	            }
+	            if (isincreasing) {
+	                System.err.println("Yes");
+	            } else {
+	                System.out.println("No");
+	            }
+
+	        }
 	}
 
 }
